c9b48efd11bb7e7350f46f12bddf8053510d2763
Abdulla Al Muhit <muhitabdullah279@gmail.com>
2024-09-22 20:46:21 +0530

    Final booking alert message has modified.

--- js/nextButton.js
+++ js/nextButton.js
@@ -3,8 +3,5 @@ document.getElementById('book-next-btn').addEventListener('click', function(even
 
     const passegerName = document.getElementById('passenger-name').value;
 
-    alert(`
-        Thank you ${passegerName} for booking tickets from us! 
-        Shortly you will find a confirmation in your email.
-        `);
+    alert(`Thank you ${passegerName} for booking tickets from us! Shortly you will find a confirmation in your email.`);
 })
