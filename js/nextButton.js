b6ceec8889bbb46c12544ad38eb5b116b88fcba9
Abdulla Al Muhit <muhitabdullah279@gmail.com>
2024-09-24 02:14:36 +0530

    Booking validation has modified.

--- js/nextButton.js
+++ js/nextButton.js
@@ -1,26 +1,45 @@
-function validateInfo() { 
-    document.getElementById('passenger-name').addEventListener('keyup', function(event) {
-        const name = event.target.value;
-        if (name !== "") {
-            document.getElementById('phone-no').addEventListener('keyup', function(event) {
-                const phoneNo = event.target.value;
-                if (!isNaN(phoneNo) && phoneNo.length === 11) {
-                    document.getElementById('book-next-btn').removeAttribute('disabled');
-                }
-                else {
-                    document.getElementById('book-next-btn').setAttribute('disabled', true);
-                }
-            })
-        }
-        else {
-            document.getElementById('book-next-btn').setAttribute('disabled', true);
-        }
-    })
-}
-
 document.getElementById('book-next-btn').addEventListener('click', function(event) {
     event.preventDefault();
-    document.getElementById('my_modal_1').showModal();
+
+    const totalBooked = parseInt(document.getElementById('total-booked').innerText);
+    const passengerName = document.getElementById('passenger-name').value;
+    const phoneNo = document.getElementById('phone-no').value;
+    
+    let nameValidate = false;
+    let phoneValidate = false;
+    
+    if (passengerName !== "") {
+        const passengerNameCh = passengerName.split('');
+        for (const element of passengerNameCh) {
+            if (isNaN(element)) {
+                nameValidate = true;
+            } else {
+                nameValidate = false;
+            }
+        }
+    } else{
+        nameValidate = false;
+    }
+
+    if (phoneNo !== "") {
+        if (phoneNo.length >= 11 && !isNaN(phoneNo)) {
+            phoneValidate = true;
+        } else {
+            phoneValidate = false;
+        }
+    } else {
+        phoneValidate = false;
+    }
+
+    if (totalBooked > 0) {
+        if (nameValidate && phoneValidate) {
+            document.getElementById('my_modal_1').showModal();
+        } else {
+            alert("Invalid passenger name or phone number input.");
+        }  
+    } else {
+        alert('Please select at least one seat to confirm your booking.')
+    }
 })
 
 document.getElementById('modal-btn').addEventListener('click', function() {
--- js/seatSelect.js
+++ js/seatSelect.js
@@ -39,10 +39,6 @@ function clickSeat(event) {
                 totalPrice += 550;
                 document.getElementById('total-price').innerText = String(totalPrice);
                 document.getElementById('grand-total').innerText = String(totalPrice);
-
-                if (count > 0) {
-                    validateInfo();
-                }
     
                 return;
             }
